5f3a266249a905d41b911c402e7a448de112e24d
Michael Babienco <mbabienco@southern.edu>
2025-10-05 10:30:37 +0900

    Add test for sanity check on overrides

--- src/NetSparkle.Tests.AppCastGenerator/OptionsTests.cs
+++ src/NetSparkle.Tests.AppCastGenerator/OptionsTests.cs
@@ -7,6 +7,21 @@ namespace NetSparkle.Tests.AppCastGenerator
     public class OptionsTests
     {
         [Fact]
+        public void ParseArguments_NoKeyOverrideInitially()
+        {
+            var args = new[]
+            {
+                "--generate-signature", "binary.exe",
+            };
+
+            var result = Parser.Default.ParseArguments<Options>(args);
+
+            Assert.Empty(result.Errors);
+            Assert.Equal("", result.Value.PublicKeyOverride);
+            Assert.Equal("", result.Value.PrivateKeyOverride);
+        }
+
+        [Fact]
         public void ParseArguments_KeysOverrideAndGenerateSignature_Compatibility()
         {
             var args = new[]
